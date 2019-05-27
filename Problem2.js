f4200b97e948437fa7c13fe2b015c0300fae93cd
Ilko Mihov <ilko@Ilkos-MacBook-Pro.local>
2019-05-26 21:58:44 -0400

    Assignment #1

--- Problem2.js
+++ Problem2.js
@@ -1,6 +1,6 @@
 const evaluate = (string) => calculate[string[1]](string[0],string[2])
 const calculate = {
-    '+': function(left,right) {return left+right},
+    '+': function(left,right) {return parseInt(left,10)+parseInt(right,10)},
     '-': function(left,right) {return left-right},
     '*': function(left,right) {return left*right},
     '/': function(left,right) {return left/right},
@@ -12,8 +12,13 @@ const expression2 = '8/3';
 const expression3 = '8-3';
 const expression4 = '8+3';
 
-console.log(`${expression} = ${evaluate(expression)}`)
-console.log(`${expression} = ${evaluate(expression1)}`)
-console.log(`${expression} = ${evaluate(expression2)}`)
-console.log(`${expression} = ${evaluate(expression3)}`)
-console.log(`${expression} = ${evaluate(expression4)}`)
+let operator = evaluate(expression)
+console.log(`${expression} = ${operator}`)
+let operator1 = evaluate(expression1)
+console.log(`${expression1} = ${operator1}`)
+let operator2 = evaluate(expression2)
+console.log(`${expression2} = ${operator2}`)
+let operator3 = evaluate(expression)
+console.log(`${expression3} = ${operator3}`)
+let operator4 = evaluate(expression4)
+console.log(`${expression4} = ${operator4}`)
