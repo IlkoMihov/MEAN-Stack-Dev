08f70ed52f63b90e200b2f1f16486e407fe5fced
Ilko Mihov <yumen50@gmail.com>
2019-06-08 22:49:04 -0400

    Update ps4.js

--- routes/ps4.js
+++ routes/ps4.js
@@ -25,28 +25,6 @@ const getPeopleInfo = () =>{
         reject(e.message)
       });
     })})}
-const getStationInfo = () =>{
-  return new Promise ((resolve, reject) => {
-    var req = http.get('http://api.open-notify.org/iss-now.json', (res) => {
-       let rawData = []
-
-      res.on('data', (chunk) => {
-        rawData.push(chunk)
-      });
-
-      res.on('end', () => {
-        try {
-          const parsedData = JSON.parse(rawData);
-          resolve(parsedData);
-        } catch (e) {
-          reject(e.message);
-        }
-      });
-      req.on('error', function(e) {
-        console.log('ERROR: ' + e.message);
-        reject(e.message)
-      });
-    })})}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   getPeopleInfo().then((parsedData)=>{
