a7225de8269242629f83a76d4101959a5cc30d82
Ilko Mihov <yumen50@gmail.com>
2019-06-18 21:44:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 
+
 var indexRouter = require('./routes/ps4');
 var app = express();
 
@@ -11,6 +12,13 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/ps4', indexRouter);
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next()
+})
+
+app.use('/', indexRouter);
 
 module.exports = app;
